3b67fb657399233154bdded5e61e158914424eb4
Audrey Carlsen <acarlsen@seattletimes.com>
2015-01-27 12:40:16 -0800

    Adds form as modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 // //Use CommonJS style via browserify to load other modules
 
 require("./form");
+var $ = require("jquery");
+
+$(".close").on("click", function() {
+  $(".overlay").removeClass("visible");
+});
