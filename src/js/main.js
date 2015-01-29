41f498b5dad3257db0a6284d668b781a9f90194d
Audrey Carlsen <acarlsen@seattletimes.com>
2015-01-29 12:40:58 -0800

    Tooltip works on click for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,13 @@ $(".add-yourself").on("click", function() {
   $(".results").hide();
   $(".form-container").show();
 });
+
+$(".point").on("click", function(e) {
+  e.stopPropagation();
+  $(".click-tooltip").removeClass("click-tooltip");
+  $(e.target).next().addClass("click-tooltip");
+});
+
+$(".scatter-box-outer").on("click", function(e) {
+  $(".click-tooltip").removeClass("click-tooltip");
+});
